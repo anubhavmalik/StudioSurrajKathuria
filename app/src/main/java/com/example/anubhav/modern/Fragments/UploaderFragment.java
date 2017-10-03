2fbced219bf4b2acfc8940ee2119224897e9368b
anubhavmalik <anubhavmalikdeveloper@gmail.com>
2017-10-04 02:33:51 +0530

    working on storage in next update

--- app/src/main/java/com/example/anubhav/modern/Fragments/UploaderFragment.java
+++ app/src/main/java/com/example/anubhav/modern/Fragments/UploaderFragment.java
@@ -1,5 +1,6 @@
 package com.example.anubhav.modern.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -20,13 +21,12 @@ import de.hdodenhof.circleimageview.CircleImageView;
  */
 
 public class UploaderFragment extends Fragment {
+    final int RC_PHOTO_PICKER = 26;
     EditText detailsEditText;
     FloatingActionButton uploaderFab;
     ImageView mPhotoPickerButton;
     TextView nameTextView;
     CircleImageView circleImageView;
-
-
 
     @Nullable
     @Override
@@ -34,6 +34,19 @@ public class UploaderFragment extends Fragment {
         View v = inflater.inflate(R.layout.activity_uploader, null);
         detailsEditText = v.findViewById(R.id.uploader_editText);
         circleImageView = v.findViewById(R.id.uploader_circleImageView);
+        uploaderFab = v.findViewById(R.id.uploader_fab);
+        nameTextView = v.findViewById(R.id.uploader_nameTextView);
+        mPhotoPickerButton = v.findViewById(R.id.uploader_imageView);
+
+        mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
+            }
+        });
 
         return v;
     }
